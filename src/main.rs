305efecb33ac53a7381448810e566da933dc9337
xiyuxx <1522189866@qq.com>
2024-04-27 14:28:53 +0800

    fix: now main.rs and Cargo.toml can be accessed

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#[macro_use] extern crate rocket;
+
+
+
+use gd_backend::auth::route::{add_user, create_user, edit, login};
+use gd_backend::config::{get_custom_figment, init_my_config};
+use gd_backend::db::init_gd_data;
+use gd_backend::project::route::{add_workmate_to_project, delete_project, get_participants, get_project, set_project};
+use gd_backend::project::work_item::route::{get_all_item, set_item};
+use gd_backend::state::{get_default_user_token, init_validate_instance};
+use crate::catcher::{bad_request_catcher, error_catcher, not_found_catcher};
+
+mod catcher;
+
+#[launch]
+fn rocket()-> _ {
+    rocket::custom(get_custom_figment())
+        .attach(init_my_config())
+        .attach(init_gd_data())
+        .attach(get_default_user_token())
+        .manage(init_validate_instance().unwrap())
+        .register("/",catchers![error_catcher,not_found_catcher,bad_request_catcher])
+        .mount("/user",routes![add_user,create_user,login,edit])
+        .mount("/project",routes![set_project,get_project,delete_project,
+            add_workmate_to_project,get_participants])
+        .mount("/item",routes![set_item,get_all_item])
+
+}
